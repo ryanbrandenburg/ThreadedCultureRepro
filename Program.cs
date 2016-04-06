e10386d9d98bd0fef3aa90259903d104ab8115fe
Ryan Brandenburg <rybrande@microsoft.com>
2016-04-06 10:27:50 -0700

    Threaded Culture bug repro

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading;
+using System.Globalization;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            MainAsync().Wait();
+        }
+        
+        static async Task MainAsync()
+        {
+            CultureInfo.CurrentCulture = new CultureInfo("fr");
+            var threadId = Thread.CurrentThread.ManagedThreadId;
+            Console.WriteLine($"ThreadId: {threadId} CurrentCulture: {CultureInfo.CurrentCulture}.");
+            await Task.Delay(500);
+            threadId = Thread.CurrentThread.ManagedThreadId;
+            Console.WriteLine($"ThreadId: {threadId} CurrentCulture: {CultureInfo.CurrentCulture}.");
+        }
+    }
+}
